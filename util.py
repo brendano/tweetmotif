39a53f2c2a66f8f53e91cf3dda5447ed107f1cf9
davidahn <davidahn@d30015d2-e6b2-423d-b417-961bdb47cc32>
2009-05-02 22:49:55 +0000

    Adding levenshtein distance for lists.
    git-svn-id: svn+ssh://anyall.org/home/svn/twi@75 d30015d2-e6b2-423d-b417-961bdb47cc32

--- util.py
+++ util.py
@@ -370,3 +370,25 @@ if 0:  #__name__=='__main__':
   for x in counter(range(100)):
     time.sleep(0.1)
 
+# From http://en.wikisource.org/wiki/Levenshtein_distance
+# Python version by Magnus Lie Hetland
+
+def levenshtein_distance(a,b):
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a,b = b,a
+        n,m = m,n
+        
+    current = range(n+1)
+    for i in range(1,m+1):
+        previous, current = current, [i]+[0]*m
+        for j in range(1,n+1):
+            add, delete = previous[j]+1, current[j-1]+1
+            change = previous[j-1]
+            if a[j-1] != b[i-1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+            
+    return current[n]
