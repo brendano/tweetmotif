8634a74ce47164ca49ad8ddef481c047d906d3c5
mikeyk <mikeyk@d30015d2-e6b2-423d-b417-961bdb47cc32>
2009-05-03 00:13:09 +0000

    adding django frontend
    git-svn-id: svn+ssh://anyall.org/home/svn/twi@81 d30015d2-e6b2-423d-b417-961bdb47cc32

---
+++ django/twi/views.py
@@ -0,0 +1,29 @@
+# Create your views here.
+
+import sys,urllib2
+# sys.path.insert(0, "/Users/mkrieger/src/twi")
+import cPickle as pickle
+from django.http import HttpResponse
+from django.template import loader, RequestContext
+import simplejson
+
+def index(request):
+	"""docstring for index"""
+	t = loader.get_template("index.tpl")
+	c = RequestContext(request, {})
+	return HttpResponse(t.render(c))
+	
+def do_query(request):
+	"""docstring for do_query"""
+	if not "q" in request.REQUEST:
+		return HttpResponse("No query")
+	else:
+		s=urllib2.urlopen("http://localhost:8080/?q=%s&format=pickle" % request.REQUEST['q']).read()
+		topic_results=pickle.loads(s)
+		bigass_topic_dict = dict((t['label'], dict(
+		  label=t['label'], 
+		  nice_tweets=t['nice_tweets'], 
+		  tweet_ids=t['tweet_ids'],
+		)) for t in topic_results)		
+		topic_list = [x['label'] for x in topic_results]
+		return HttpResponse(simplejson.dumps({"topic_list":topic_list, "topic_info": bigass_topic_dict}))
