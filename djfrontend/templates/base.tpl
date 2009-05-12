2b32571c8f4b599c52b6878aa9f31c248b794730
mikeyk <mikeyk@d30015d2-e6b2-423d-b417-961bdb47cc32>
2009-05-12 08:25:48 +0000

    adding twitter prof icon
    git-svn-id: svn+ssh://anyall.org/home/svn/twi@125 d30015d2-e6b2-423d-b417-961bdb47cc32

--- djfrontend/templates/base.tpl
+++ djfrontend/templates/base.tpl
@@ -9,10 +9,13 @@
       }
     </script>
     <script src="static/js/jquery-1.3.2.min.js" type="text/javascript" charset="utf-8"></script>
-    <script type="text/javascript" charset="utf-8">
+		<script src="static/js/motif.js" type="text/javascript" charset="utf-8"></script>
+		
+		{% block extrahead %}
 
-    </script>
-    <link rel="stylesheet" href="static/css/trends.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+		{% endblock %}
+		
+    <link rel="stylesheet" href="static/css/trends-general.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
   </head>
   <body>  
     {% block content %}
--- djfrontend/twi/views.py
+++ djfrontend/twi/views.py
@@ -2,13 +2,36 @@ import sys,os,urllib2
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader, RequestContext
 import simplejson
-
+import re
+from django.shortcuts import render_to_response
 import trends, myurl, util
 import timeout_urllib2; timeout_urllib2.sethttptimeout(20.0)
 
+
+_IPHONE_UA = re.compile(r'Mobile.*Safari')
+def is_iphone(request):
+    return True
+    return _IPHONE_UA.search(request.META['HTTP_USER_AGENT']) is not None
+
+def response(template,*args):
+    def _process_view(view_func):
+        def _handle_request(request,*args,**kwargs):
+            context = view_func(request,*args,**kwargs)
+            if context is None: context = dict()
+            if isinstance(context,dict):
+                path = is_iphone(request) and 'iphone' or 'web'
+                return render_to_response('%s/%s' % (path,template),RequestContext(request,dict=context))
+            else:
+                return context #this is probably a redirect
+
+        return _handle_request
+    return _process_view
+
+
 BACKEND_URL = "http://localhost:8080" if sys.platform=='darwin' else "http://localhost/twitterthemes/backend"
+
+@response('index.tpl')
 def index(request):
-  t = loader.get_template("index.tpl")
   d = {}
   d['trend_topics'] = trends.current_topics()
   d['default_query'] = ''  #d['trend_topics'][0]['name'] if d['trend_topics'] else "sandwich"
@@ -17,16 +40,20 @@ def index(request):
     x['simple_query'] = ('"%s"' % x['name']) if len(x['name'].split())>1 else x['name']
   d['prebaked_queries'] = ['sandwich', 'coffee', ':)', ':(', 'aw', 'awwwwww', 'school', 'jobs']
   c = RequestContext(request, d)
-  return HttpResponse(t.render(c))
-	  
+  return d
+      
 
 def show_results(request, query):
-	return HttpResponseRedirect("/#" + query)
+    return HttpResponseRedirect("/#" + query)
 
 def do_query(request):
   if not "q" in request.REQUEST:
     return HttpResponse("No query")
   else:
     q = util.stringify(myurl.quote(request.REQUEST['q']))
-    json = urllib2.urlopen(BACKEND_URL + "/?q=%s&format=json" % q).read()
+    max_topics = request.REQUEST.get("max_topics", 40)
+    json = urllib2.urlopen(BACKEND_URL + "/?q=%s&max_topics=%s&format=json" % (q, max_topics)).read()
     return HttpResponse(json)
+
+
+
